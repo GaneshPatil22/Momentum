8af29ebca9900abead4e96562981d8197d25dfd4
Ganesh Patil <ganesh@mtailor.com>
2026-06-24 19:41:47 +0530

    Initial Commit

---
+++ Momentum/MomentumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumApp.swift
+//  Momentum
+//
+//  Created by Ganesh Patil on 24/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MomentumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
